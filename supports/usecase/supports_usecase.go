6c100d075e0405df7185f733cf65621c94c02037
Anton <apriyma0@gmail.com>
2019-11-23 14:49:20 +0300

    [ADD]Sessions

--- supports/usecase/supports_usecase.go
+++ supports/usecase/supports_usecase.go
@@ -12,10 +12,12 @@ import (
 type SupportsUseCase interface {
 	GetSupportByID(id uint64) (models.Support, error)
 	Login(support models.Support) (models.Support, error)
+	GetUserBySession(session string) (uint64,error)
 }
 
 type supportUseCase struct {
 	repository repository.SupportRepo
+	sessions repository.SessionRepository
 }
 
 func (u *supportUseCase) Login(loginSupport models.Support) (models.Support, error) {
@@ -34,9 +36,10 @@ func (u *supportUseCase) Login(loginSupport models.Support) (models.Support, err
 
 }
 
-func NewSupportUseCase(repo repository.SupportRepo) SupportsUseCase {
+func NewSupportUseCase(repo repository.SupportRepo,sessions repository.SessionRepository) SupportsUseCase {
 	return &supportUseCase{
 		repository: repo,
+		sessions:sessions,
 	}
 }
 
@@ -62,6 +65,12 @@ func comparePasswords(hashedPassword string, plainPassword string) bool {
 		return false
 	}
 	return true
+}
+
+func (u *supportUseCase) GetUserBySession(session string) (uint64,error) {
+	id,err:=u.sessions.GetID(session)
+	return id, err
+
 }
 
 //TODO: use on support creation
