2604c5f2ca68c5c9405688dcd560506b6977244f
Anton <apriyma0@gmail.com>
2019-11-23 16:09:19 +0300

    [ADD] /chats

--- chats/usecase/chats_usecase.go
+++ chats/usecase/chats_usecase.go
@@ -3,16 +3,30 @@ package usecase
 import (
 	"github.com/CoolCodeTeam/CoolSupportBackend/chats/models"
 	"github.com/CoolCodeTeam/CoolSupportBackend/chats/repository"
+	users"github.com/CoolCodeTeam/CoolSupportBackend/supports/usecase"
 )
 
 type ChatsUseCase interface {
 	GetChatsByUserID(ID uint64) ([]models.Chat, error)
 	RemoveChat(ID uint64) error
 	CreateChat(suppID uint64) (uint64,error)
+	GetChat() (uint64 ,error)
 }
 
 type ChatsUseCaseImpl struct {
 	repository repository.ChatsRepository
+	users users.SupportsUseCase
+}
+
+func (c *ChatsUseCaseImpl) GetChat() (uint64, error) {
+	//get random user_id
+	randomID,err:=c.users.GetRandomID()
+	if err!=nil{
+		return 0,err
+	}
+	//create_chat
+	id,err:=c.CreateChat(randomID)
+	return id,err
 }
 
 func (c *ChatsUseCaseImpl) RemoveChat(ID uint64) error {
